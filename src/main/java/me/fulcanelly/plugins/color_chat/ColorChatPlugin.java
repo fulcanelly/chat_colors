6dde75c2d7352cfdc29d3b385796992d0ba82242
fulcanelly <dallasxfulcanelly@gmail.com>
2025-04-06 14:51:49 +0300

    fix: fix colors

--- src/main/java/me/fulcanelly/plugins/color_chat/ColorChatPlugin.java
+++ src/main/java/me/fulcanelly/plugins/color_chat/ColorChatPlugin.java
@@ -2,37 +2,38 @@ package me.fulcanelly.plugins.color_chat;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ColorChatPlugin extends JavaPlugin implements Listener {
 
-    private String[] allMinecraftChatColors = {
-        "&0", // Black
-        "&1", // Dark Blue
-        "&2", // Dark Green
-        "&3", // Dark Aqua
-        "&4", // Dark Red
-        "&5", // Dark Purple
-        "&6", // Gold
-        "&7", // Gray
-        "&8", // Dark Gray
-        "&9", // Blue
-        "&a", // Green
-        "&b", // Aqua
-        "&c", // Red
-        "&d", // Light Purple
-        "&e", // Yellow
-        "&f"  // White
+    private ChatColor[] allMinecraftChatColors = {
+        ChatColor.BLACK,      // Black
+        ChatColor.DARK_BLUE,  // Dark Blue
+        ChatColor.DARK_GREEN, // Dark Green
+        ChatColor.DARK_AQUA,  // Dark Aqua
+        ChatColor.DARK_RED,   // Dark Red
+        ChatColor.DARK_PURPLE,// Dark Purple
+        ChatColor.GOLD,       // Gold
+        ChatColor.GRAY,       // Gray
+        ChatColor.DARK_GRAY,  // Dark Gray
+        ChatColor.BLUE,       // Blue
+        ChatColor.GREEN,      // Green
+        ChatColor.AQUA,       // Aqua
+        ChatColor.RED,        // Red
+        ChatColor.LIGHT_PURPLE,// Light Purple
+        ChatColor.YELLOW,     // Yellow
+        ChatColor.WHITE       // White
     };
 
-    private String[] specialChatColors = {
-        "&k", // Obfuscated
-        "&l", // Bold
-        "&m", // Strikethrough
-        "&n", // Underline
-        "&o", // Italic
-        "&r"  // Reset
+    private ChatColor[] specialChatColors = {
+        ChatColor.MAGIC,      // Obfuscated
+        ChatColor.BOLD,       // Bold
+        ChatColor.STRIKETHROUGH, // Strikethrough
+        ChatColor.UNDERLINE,  // Underline
+        ChatColor.ITALIC,     // Italic
+        ChatColor.RESET       // Reset
     };
 
     @Override
@@ -51,7 +52,7 @@ public class ColorChatPlugin extends JavaPlugin implements Listener {
         int playerNameHash = e.getPlayer().getName().hashCode();
 
         int colorIndex = Math.abs(playerNameHash % allMinecraftChatColors.length);
-        String color = allMinecraftChatColors[colorIndex];
+        ChatColor color = allMinecraftChatColors[colorIndex];
 
         e.setMessage(color + e.getMessage());
     }
